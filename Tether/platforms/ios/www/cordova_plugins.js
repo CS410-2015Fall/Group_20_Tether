0a8a1a7a01c43b01b0e5fcb984e128161115057d
tunjayjafarli <tuncay_ceferli@live.com>
2015-10-30 17:19:01 -0700

    added appList plugin & contract setup

--- Tether/platforms/ios/www/cordova_plugins.js
+++ Tether/platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.jmrezayi2.Applist/www/Applist.js",
+        "id": "org.jmrezayi2.Applist.Applist",
+        "clobbers": [
+            "window.Applist"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.jmrezayi2.Applist": "0.1.4"
 }
 // BOTTOM OF METADATA
 });
