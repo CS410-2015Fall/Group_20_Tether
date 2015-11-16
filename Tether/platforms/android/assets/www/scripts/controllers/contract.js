fbabd4666b69c67979f6524f20951ea4b8353c15
tunjayjafarli <tuncay_ceferli@live.com>
2015-11-16 15:07:47 -0800

    redone wager amount [conflicts]

--- Tether/platforms/android/assets/www/scripts/controllers/contract.js
+++ Tether/platforms/android/assets/www/scripts/controllers/contract.js
@@ -9,32 +9,33 @@ angular.module('tetherApp')
         // send the token to server need better place for this...
         // commnet out this section if it cause errors I implemented something in the server
         // or try copying TetherDjango into your venv and run the server
-        var token = $window.localStorage.gcmtoken;
-        var data = {
-            'gcm_token':token
-        }
-        userService.updateProfile(data)
-            .then(function(data){
-                // success case
-                $scope.complete = true;
-            },function(data){
-                // error case
-                $scope.error = data;
-            });
+        //var token = $window.localStorage.gcmtoken;
+        //var data = {
+        //    'gcm_token':token
+        //}
+        //userService.updateProfile(data)
+        //   .then(function(data){
+        //        // success case
+        //        $scope.complete = true;
+        //    },function(data){
+        //        // error case
+        //        $scope.error = data;
+        //    });
         /////////////////////////////////////////////////////////////////////////
         $scope.showButton = true;
         $scope.submitted = false;
 
         $scope.validHours = true;
         $scope.validAppSelection = true;
-
+        $scope.validWagerAmount = true;
 
         $scope.contractHours = 0;
         $scope.contractMinutes = 0;
         $scope.contractSeconds = 0;
 
+        $scope.wagerAmount = 0;
 
-        //Scope variables for monitoring
+       //Scope variables for monitoring
         $scope.blacklistedApps = [];
         $scope.foregroundApp = "";
         $scope.blacklistedAppUsed = "";
@@ -57,7 +58,7 @@ angular.module('tetherApp')
         });
 
 
-        var contractJSON = '{"contract":{"apps":[],"durationInMins":0}}';
+        var contractJSON = '{"contract":{"apps":[],"durationInMins":0,"wagerAmount":0}}';
 
         $scope.submitContract = function(){
 
@@ -66,6 +67,7 @@ angular.module('tetherApp')
             $scope.blacklistedApps = [];
             $scope.validHours = true;
             $scope.validAppSelection = true;
+            $scope.validWagerAmount = true;
 
             var globalForegroundApp = "";
 
@@ -86,6 +88,9 @@ angular.module('tetherApp')
             var durationMins = parseInt(document.getElementById("minID").value);
             var durationSecs = parseInt(document.getElementById("secID").value);
 
+            // parse wagering amount input
+            $scope.wagerAmount = parseInt(document.getElementById("wagerID").value);
+
             if (isNaN(durationHrs)){
                 durationHrs = 0;
                 $scope.contractHours = 0;
@@ -101,12 +106,10 @@ angular.module('tetherApp')
                 $scope.contractSeconds = 0;
             }
 
-            //trying this -> test
+            if (isNaN($scope.wagerAmount)){
+                $scope.wagerAmount = 0;
+            }
 
-           // if ($scope.blacklistedApps = []){
-              //  console.log("User didn't choose any apps");
-               // $scope.validAppSelection = false;
-            //}
 
             if ($scope.blacklistedApps.length == 0){
                 console.log("User hasn't selected any apps");
@@ -115,23 +118,26 @@ angular.module('tetherApp')
 
             if (!$scope.validateHours(durationHrs, durationMins, durationSecs)){
                 console.log("Hour input denied");
-
                 $scope.validHours = false;
-
             }
 
-            if ($scope.validHours == false || $scope.validAppSelection == false){
+            if ($scope.wagerAmount == 0){
+                console.log("Wagering amount is not valid");
+                $scope.validWagerAmount = false;
+            }
+
+            if ($scope.validHours == false || $scope.validAppSelection == false || $scope.validWagerAmount == false){
                 return;
             } else {
                 // convert to minutes and attach to contract JSON object
                 var durationInMins = (durationHrs * 60) + durationMins + (durationSecs / 60);
                 obj["contract"].durationInMins = durationInMins;
                 $scope.validHours = true;
+
+                // attach wagering amount to contract JSON object
+                obj["contract"].wagerAmount = $scope.wagerAmount;
+                $scope.validWagerAmount = true;
             }
-
-
-
-
 
             contractJSON = JSON.stringify(obj);
             console.log(JSON.stringify(contractJSON));
@@ -141,8 +147,6 @@ angular.module('tetherApp')
             $scope.contractOver = false;
 
             $scope.startTimer();
-
-
 
         };
 
@@ -198,6 +202,7 @@ angular.module('tetherApp')
                 $scope.ongoingContract = false;
                 $scope.contractOver = true;
                 $scope.contractSuccess = true;
+                $scope.contractForfeited = false;
                 $scope.blacklistedApps = [];
                 // local notification
                 navigator.notification.alert('You have successfully completed contract!');
@@ -215,6 +220,7 @@ angular.module('tetherApp')
                 $scope.ongoingContract = false;
                 $scope.contractOver = true;
                 $scope.contractSuccess = false;
+                $scope.contractForfeited = true;
                 $scope.blacklistedApps = [];
                 clearInterval($scope.refreshContractTimerIntervalId);
                 // local notification
@@ -232,6 +238,7 @@ angular.module('tetherApp')
                 $scope.ongoingContract = false;
                 $scope.contractOver = true;
                 $scope.contractSuccess = false;
+                $scope.contractForfeited = false;
                 $scope.blacklistedApps = [];
                 clearInterval($scope.refreshContractTimerIntervalId);
                 // local notification
@@ -285,19 +292,20 @@ angular.module('tetherApp')
 
         $scope.validHours = true;
         $scope.validAppSelection = true;
-
+        $scope.validWagerAmount = true;
 
         $scope.contractHours = 0;
         $scope.contractMinutes = 0;
         $scope.contractSeconds = 0;
 
+        $scope.wagerAmount = 0;
 
         //Scope variables for monitoring
         $scope.blacklistedApps = [];
         $scope.foregroundApp = "";
         $scope.blacklistedAppUsed = "";
 
-            $location.path('/');
+        $location.path('/');
         };
 
         $scope.routeToContract = function(){
@@ -306,12 +314,13 @@ angular.module('tetherApp')
 
             $scope.validHours = true;
             $scope.validAppSelection = true;
-
+            $scope.validWagerAmount = true;
 
             $scope.contractHours = 0;
             $scope.contractMinutes = 0;
             $scope.contractSeconds = 0;
 
+            $scope.wagerAmount = 0;
 
             //Scope variables for monitoring
             $scope.blacklistedApps = [];
@@ -323,17 +332,8 @@ angular.module('tetherApp')
             contractService.applist();
         };
 
-
-
-
-
         // add event listener for back button if ongoing contract is true pops up toast saying they can't use the app
         //since it will distract them
-
-
-        /*
-         $scope.contract = [contract: {apps:[ {name: "FB", img: xxx}, {name: "Google", img:yyy}], durationInMins:"30"}]
-         */
 
     });
 
