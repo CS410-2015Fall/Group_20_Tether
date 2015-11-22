8dadc4cc308e4102f9a891dd60d2897fda0e4e81
lanepither <lanepither@hotmail.com>
2015-11-21 18:10:16 -0800

    Home stuff and friends contract

--- Tether/www/scripts/app.js
+++ Tether/www/scripts/app.js
@@ -127,7 +127,7 @@ angular.module('tetherApp', [
             });
     })
   .run(function(userService){
-    userService.initialize('http://128.189.250.150:8000/rest-auth', false);
+    userService.initialize('http://128.189.254.186:8000/rest-auth', false);
     //    userService.initialize('http://192.168.1.67:8000/rest-auth', false);
 
 
